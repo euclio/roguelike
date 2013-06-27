31565c574e511946f92bdd43017dc3a41dc6e8da
Xander Masotto <xanderm@google.com>
2013-06-27 02:04:07 -0700

    inventory

--- game/system/rgame.cc
+++ game/system/rgame.cc
@@ -1,10 +1,70 @@
 #include <boost/python.hpp>
 #include <ncurses.h>
+#include <algorithm>
+#include <string>
+#include <vector>
 
 using namespace boost::python;
 
 char GetTileChar(object tile) {
   return '.';
+}
+
+void RenderRectangle(int x, int y, int width, int height,
+                     const std::string &title) {
+  for (int ix = x; ix < x + width; ix++) {
+    for (int iy = y; iy < y + height; iy++) {
+      mvaddch(iy, ix, ' ');
+    }
+  }
+  for (int iy = y; iy < y + height; iy++) {
+    mvaddch(iy, x, '*');
+    mvaddch(iy, x + width, '*');
+  }
+  for (int ix = x; ix <= x + width; ix++) {
+    mvaddch(y, ix, '=');
+    mvaddch(y + height, ix, '=');
+  }
+  move(y, x+2);
+  printw(title.c_str());
+}
+
+int ListMaxWidth(const std::vector<std::string> &lines) {
+  int maxWidth = 0;
+  for (int i=0; i<lines.size(); i++) {
+    if (lines[i].size() > maxWidth) {
+      maxWidth = lines[i].size();
+    }
+  }
+  return maxWidth;
+}
+
+void RenderRectangleList(int x, int y, const std::string &title,
+                         const std::vector<std::string> &lines) {
+  int width = ListMaxWidth(lines) + 5;
+  int height = lines.size() + 1;
+  RenderRectangle(x, y, width, height, title);
+  for (int i = 0; i < lines.size(); i++) {
+    move(y + 1 + i, x + 2);
+    printw(lines[i].c_str());
+  }
+}
+
+void RenderInventory() {
+  std::vector<std::string> lines;
+  lines.push_back("item 1");
+  lines.push_back("item 2 hahahhahahaha");
+  lines.push_back("item 3");
+  lines.push_back("item 4");
+
+  for (int i=0; i<lines.size(); i++) {
+    lines[i].insert(0, 4, ' ');
+    lines[i][0] = '[';
+    lines[i][1] = 'a' + i;
+    lines[i][2] = ']';
+  }
+
+  RenderRectangleList(20, 5, "Inventory", lines);
 }
 
 void Render(object gamestate) {
@@ -25,6 +85,8 @@ void Render(object gamestate) {
   object player = gamestate.attr("player");
   mvaddch(extract<int>(player.attr("location")[0]),
           extract<int>(player.attr("location")[1]), '@');
+
+  //RenderInventory();
 }
 
 void GameInit(object world, object inputHandler) {
