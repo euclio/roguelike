389aeba388e48978f2ff0714ef8359eae2390794
Xander Masotto <xanderm@google.com>
2013-06-26 22:12:47 -0700

    basic c++ infrastructure

---
+++ game/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from system.rgame import *
+
+class World:
+  def __init__(self):
+    self.message = "default message"
+
+world = World()
+
+def inputHandler(ch):
+  world.message = ch
+
+# Hand over control to C++ right away.
+# world: A world object, containing all data structures
+#   that C++ needs to render
+# inputHandler: A function for reacting to the input from
+#   a keypress
+GameInit(world, inputHandler)
