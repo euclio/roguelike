51d201c9bb1346b21a3de9472020943d70433ddf
Shashwat Lal Das <me@thisemail.com>
2013-06-27 01:12:22 -0700

    Added player class and implemented a dummy action.

--- game/entity.py
+++ game/entity.py
@@ -19,20 +19,12 @@ WEARABLE_ENTITY_CLASS = EntityClass("wearable")
 WEAPON_ENTITY_CLASS = EntityClass("weapon", {'attack': 10})
 EDIBLE_ENTITY_CLASS = EntityClass("edible")
 
-# Entities may boost or reduce player attributes when
-# used/wielded/carried. How do we represent this?
-
-# Also, each entity instance should have specific attributes such as
-# name, HP, hunger, gender, etc. Different kinds of entities have
-# different kinds of attributes. How do we define a 'kind of entity'?
-# EntityType + [EntityClass list]?
-
 class EntityType:
   """Each entity can only have one entity type."""
 
   def __init__(self, type_name = None, entity_classes = None):
     self.entity_classes = entity_classes
-
+    self.actions = None
     self.attributes = {}
     if self.entity_classes is not None:
       for entity_class in self.entity_classes:
@@ -76,53 +68,39 @@ class BlessedWeaponModifier(EntityModifier):
 # 1. Reduce the attack of the sword, and
 # 2. Prevent the player from dropping the sword.
 
-# class Modifier:
-#   def __init__(self):
-#     pass
-#
-#   def apply_effects(self, entity):
-#     pass
-
-# class CursedModifier(Modifier):
-#   def apply_effects(self, entity):
-#     entity.attributes['removable'] = False
-
-# class CursedWeaponModifier(CursedModifier):
-#   def apply_effects(self, entity):
-#     super.apply_effects(entity)
-#     entity.attributes['attack'] *= 0.8
-
-PLAYER_ENTITY_TYPE = EntityType("player")
 ELF_ENTITY_TYPE = EntityType("weapon")
 VELVET_ARMOR_ENTITY_TYPE = EntityType("armor")
 
 class Entity:
-  def __init__(self, entity_type = None, attributes = None, entity_modifiers = None):
+  def __init__(self, entity_type = None, entity_modifiers = None):
     self.entity_type = entity_type
-    if attributes is None:
-      self.attributes = entity_type.attributes
-    else:
-      self.attributes = attributes
+    self.actions = self.entity_type.actions
+    self.attributes = entity_type.attributes
+    self.add_modifiers(entity_modifiers)
 
+  def add_modifiers(self, entity_modifiers):
+    if entity_modifiers is None:
+      self.entity_modifiers = None
+      return
     self.entity_modifiers = entity_modifiers
-    self.update_attributes_using_entity_modifiers(entity_modifiers)
+    self.update_attributes_using_modifiers(entity_modifiers)
 
-  def update_attributes_using_entity_modifiers(self, entity_modifiers):
+  def update_attributes_using_modifiers(self, entity_modifiers):
     for entity_modifier in entity_modifiers:
       entity_modifier.apply_modifier_to_entity(self)
 
+if __name__ == "__main__":
+  # Examples
+  RANGED_WEAPON_ENTITY_CLASS = EntityClass('ranged_weapon', {'attack_range': 5,})
+  EXTRA_STRONG_WEAPON_MODIFIER = BlessedWeaponModifier("blessed_weapon_modifier")
+  CROSSBOW_ENTITY_TYPE = EntityType("crossbow", [WEAPON_ENTITY_CLASS,
+                                                 RANGED_WEAPON_ENTITY_CLASS])
 
-# Examples
-RANGED_WEAPON_ENTITY_CLASS = EntityClass('ranged_weapon', {'attack_range': 5,})
-EXTRA_STRONG_WEAPON_MODIFIER = BlessedWeaponModifier("blessed_weapon_modifier")
-CROSSBOW_ENTITY_TYPE = EntityType("crossbow", [WEAPON_ENTITY_CLASS,
-                                               RANGED_WEAPON_ENTITY_CLASS])
+  some_crossbow_entity = Entity(CROSSBOW_ENTITY_TYPE, [EXTRA_STRONG_WEAPON_MODIFIER])
 
-some_crossbow_entity = Entity(CROSSBOW_ENTITY_TYPE, None, [EXTRA_STRONG_WEAPON_MODIFIER])
-
-print
-print "Crossbow belongs to the entity classes: %s" % (map(lambda x: x.class_name, some_crossbow_entity.entity_type.entity_classes))
-print "Crossbow has the entity modifiers: %s" % (map(lambda x: x.modifier_name, some_crossbow_entity.entity_modifiers))
-print "Crossbow attack: %s" % (some_crossbow_entity.attributes['attack'])
-print "Crossbow range: %s" % (some_crossbow_entity.attributes['attack_range'])
-print "Crossbow can_drop: %s" % (some_crossbow_entity.attributes['can_drop'])
+  print
+  print "Crossbow belongs to the entity classes: %s" % (map(lambda x: x.class_name, some_crossbow_entity.entity_type.entity_classes))
+  print "Crossbow has the entity modifiers: %s" % (map(lambda x: x.modifier_name, some_crossbow_entity.entity_modifiers))
+  print "Crossbow attack: %s" % (some_crossbow_entity.attributes['attack'])
+  print "Crossbow range: %s" % (some_crossbow_entity.attributes['attack_range'])
+  print "Crossbow can_drop: %s" % (some_crossbow_entity.attributes['can_drop'])
