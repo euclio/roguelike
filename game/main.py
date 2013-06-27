209e266ba2a762f36f546577477bb62bf1b9f746
Andy Russell <arussell123@gmail.com>
2013-06-26 22:42:16 -0700

    added gameplay skeleton

--- game/main.py
+++ game/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from system.rgame import *
 from map import *
+from input import handle_input, PlayerAction
+from gameplay import handle_gameplay
 
 class World:
   def __init__(self):
@@ -9,12 +11,13 @@ class World:
 
 world = World()
 
-def inputHandler(ch):
-  world.message = ch
+def input_handler(ch):
+  action = handle_input(ch)
+  handle_gameplay(action)
 
 # Hand over control to C++ right away.
 # world: A world object, containing all data structures
 #   that C++ needs to render
 # inputHandler: A function for reacting to the input from
 #   a keypress
-GameInit(world, inputHandler)
+GameInit(world, input_handler)
